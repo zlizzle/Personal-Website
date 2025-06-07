1de42dd04a4d78edec5b74e53a205951b4d3aea3
Alex <slizzle@alexs-MacBook-Pro.local>
2025-06-07 02:46:24 -0700

    Adjusted formatting to do away with the pyramid look

--- frontend/src/pages/BlogPost.jsx
+++ frontend/src/pages/BlogPost.jsx
@@ -3,9 +3,12 @@ import { useParams, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const PostContainer = styled.div`
-  max-width: 800px;
+  max-width: 700px;
   margin: 0 auto;
-  padding: 2rem;
+  padding: 2.5rem 1.5rem 2rem 1.5rem;
+  background: #f8f6f0;
+  border-radius: 10px;
+  box-shadow: 0 1px 8px #f8e7bb40;
 `;
 
 const PostHeader = styled.header`
@@ -13,18 +16,18 @@ const PostHeader = styled.header`
 `;
 
 const PostTitle = styled.h1`
-  margin: 0 0 1rem 0;
+  margin: 0 0 0.7rem 0;
   color: #232323;
-  font-size: 2.5rem;
+  font-size: 2.3rem;
   font-weight: 800;
   letter-spacing: 0.01em;
-  text-align: center;
+  text-align: left;
 `;
 
 const PostMeta = styled.div`
   color: #6c6c6c;
   font-size: 1rem;
-  text-align: center;
+  text-align: left;
   margin-bottom: 2rem;
 `;
 
@@ -32,8 +35,6 @@ const PostContent = styled.article`
   color: #232323;
   line-height: 1.5;
   font-size: 1.13rem;
-  max-width: 700px;
-  margin: 0 auto;
   text-align: left;
 
   p {
@@ -50,8 +51,8 @@ const BackButton = styled.button`
   padding: 0.7em 1.5em;
   font-size: 1.1rem;
   font-weight: 600;
-  margin: 2rem auto 2.5rem auto;
-  display: block;
+  margin: 2rem 0 2.5rem 0;
+  display: inline-block;
   cursor: pointer;
   transition: background 0.2s;
 
