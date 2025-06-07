cb7c56ed1865b8054203ba83eecca33107c34ba2
Alex <slizzle@alexs-MacBook-Pro.local>
2025-06-06 23:24:10 -0700

    Fixed conflict betweet routers that resulted in a blank screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Blog from './pages/Blog'
+import BlogPost from './pages/BlogPost'
 import NotFound from './NotFound'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
@@ -10,6 +12,8 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:slug" element={<BlogPost />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
